431a3a05d6af875ad612aaa94d98763eec15feed
Irkatyman <tymanira@mail.ru>
2023-05-28 14:46:01 +0500

    Докоммит - базовая настройка

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React, {Suspense, FC} from 'react';
+import {BrowserRouter} from 'react-router-dom';
+import {Spin, ConfigProvider} from 'antd';
+
+import {ToastContainer} from 'react-toastify';
+
+import {RootRouter} from './routes/RootRouter';
+import 'react-toastify/scss/main.scss';
+
+export const App: FC = () => {
+    return (
+        <BrowserRouter>
+            <ConfigProvider
+                theme={{
+                    token: {
+                        fontSize: 16,
+                        fontFamily: "'IBM Plex Sans', sans-serif",
+                        fontWeightStrong: 400,
+                    },
+                }}>
+                <Suspense fallback={<Spin/>}>
+                    <div className="content">
+                        <RootRouter/>
+                    </div>
+                    <ToastContainer/>
+                </Suspense>
+            </ConfigProvider>
+        </BrowserRouter>
+    );
+}
